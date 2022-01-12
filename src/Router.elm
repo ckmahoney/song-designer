fe869bd98413a667c30a19ae837570e445af36b7
Naltroc <cortland@invisible.college>
2022-01-12 16:38:50 -0500

    Uses the encoders module

--- src/Router.elm
+++ src/Router.elm
@@ -14,6 +14,7 @@ import Http
 import Url.Builder as Url
 import Json.Decode as Decode
 import Json.Encode as Encode
+import Encoders as JE
 import File.Download as Download
 
 import ScopeEditor
@@ -155,65 +156,11 @@ getSongs email uuid =
   }
 
 
-encodeScope : Scope -> Encode.Value
-encodeScope {label, cps, cpc, root, size} =
-  Encode.object
-    [ ("label", Encode.string label)
-    , ("cps", Encode.float cps)
-    , ("root", Encode.float <| toFloat root)
-    , ("cpc", Encode.int cpc)
-    , ("size", Encode.int size)
-    ]
-
-
-encodeVoice : Voice -> Encode.Value
-encodeVoice {duty, role, label, voice, density, complexity} =
-  Encode.object
-    [ ("duty", Encode.string <| Data.dutyString duty)
-    , ("role", Encode.string <| Data.roleId role)
-    , ("label", Encode.string label)
-    , ("voice", Encode.int voice)
-    , ("density", Encode.int density)
-    , ("complexity", Encode.int complexity)
-    ]
-
-
-encodeEnsemble : Ensemble -> Encode.Value
-encodeEnsemble  =
-  Encode.list encodeVoice 
-
-
-encodeScoreMeta : ScoreMeta -> Encode.Value
-encodeScoreMeta {title, cps, root, cpc} =
-  Encode.object
-    [ ("title", Encode.string title)
-    , ("cps", Encode.float cps)
-    , ("root", Encode.float root)
-    , ("cpc", Encode.int cpc)
-    ]
-
-
-encodeCombo : Combo -> Encode.Value
-encodeCombo (scope, ensemble) =
-  Encode.object
-    [ ("scope", encodeScope scope) 
-    , ("ensemble", encodeEnsemble ensemble)
-    ]
-
-
-encodeLayout : Layout -> Encode.Value
-encodeLayout  combos =
-  Encode.object
-    [ ("title", Encode.string "ensemble")
-    , ("combos", Encode.list encodeCombo combos)
-    ]
-
-
 encodeReqTrack : String -> String -> Template -> Encode.Value
 encodeReqTrack email uuid template =
   Encode.object
-    [ ("meta", encodeScoreMeta <| Tuple.first template)
-    , ("layout", encodeLayout <| Tuple.second template)
+    [ ("meta", JE.encodeScoreMeta <| Tuple.first template)
+    , ("layout", JE.encodeLayout <| Tuple.second template)
     , ("email", Encode.string email)
     , ("uuid", Encode.string uuid)
     ]
