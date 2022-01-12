5b65cab0aba0bb319795794af63af721cddf1e34
Naltroc <cortland@invisible.college>
2022-01-12 16:37:05 -0500

    Created module to hold the JSON transforms

---
+++ src/Encoders.elm
@@ -0,0 +1,64 @@
+module Encoders exposing (..)
+
+import Types exposing (..)
+import Data
+import Json.Encode as Encode
+import Html exposing (text)
+
+
+encodeScope : Scope -> Encode.Value
+encodeScope {label, cps, cpc, root, size} =
+  Encode.object
+    [ ("label", Encode.string label)
+    , ("cps", Encode.float cps)
+    , ("root", Encode.float <| toFloat root)
+    , ("cpc", Encode.int cpc)
+    , ("size", Encode.int size)
+    ]
+
+
+encodeVoice : Voice -> Encode.Value
+encodeVoice {duty, role, label, voice, density, complexity} =
+  Encode.object
+    [ ("duty", Encode.string <| Data.dutyString duty)
+    , ("role", Encode.string <| Data.roleId role)
+    , ("label", Encode.string label)
+    , ("voice", Encode.int voice)
+    , ("density", Encode.int density)
+    , ("complexity", Encode.int complexity)
+    ]
+
+
+encodeEnsemble : Ensemble -> Encode.Value
+encodeEnsemble  =
+  Encode.list encodeVoice 
+
+
+encodeScoreMeta : ScoreMeta -> Encode.Value
+encodeScoreMeta {title, cps, root, cpc} =
+  Encode.object
+    [ ("title", Encode.string title)
+    , ("cps", Encode.float cps)
+    , ("root", Encode.float root)
+    , ("cpc", Encode.int cpc)
+    ]
+
+
+encodeCombo : Combo -> Encode.Value
+encodeCombo (scope, ensemble) =
+  Encode.object
+    [ ("scope", encodeScope scope) 
+    , ("ensemble", encodeEnsemble ensemble)
+    ]
+
+
+encodeLayout : Layout -> Encode.Value
+encodeLayout  combos =
+  Encode.object
+    [ ("title", Encode.string "ensemble")
+    , ("combos", Encode.list encodeCombo combos)
+    ]
+
+
+
+main = text ""
