4dacf7e88cf9099502f582b306454191998300b8
Naltroc <cortland@invisible.college>
2022-04-19 16:34:10 -0400

    You can edit all the parts of the arc editor

--- src/Card.elm
+++ src/Card.elm
@@ -25,7 +25,8 @@ import Playback
 
 type alias Title = String
 type alias Key = Int
-type alias Tempo = Float
+type alias Tempo = Float -- string representation of the BPM
+type alias Bpm = Float
 
 useSharps = False
 
@@ -38,7 +39,8 @@ type Style
 
 type alias Item =
   { title : String
-  , cps : Tempo
+  , tempo : String
+  , bpm : Bpm
   , key : Key
   , style : Style
   }
@@ -49,8 +51,7 @@ type Msg
   | KillEdits  
 
   | SetTitle String
-  | SetSize Int
-  | SetTempo Float
+  | SetBpm String
   | SetKey Int
   | SetStyle Style
 
@@ -59,17 +60,17 @@ type Model
   = Viewing Item 
   | Editing Item Item
 
-cpsMin = 11/15
-cpsMax = 90/15
+bpmMin = 44.0
+bpmMax = 360.0
 
 new : Item
 new = 
-  Item "New Arc" 1.9 -2 Mix
+  Item "New Arc" "104" 104 -2 Mix
 
 
 new2 : Item
 new2 = 
-  Item "New Arc 2" 2.8 -1 Beat
+  Item "New Arc 2" "144" 144 -1 Beat
 
 
 styleLabel : Style -> String
@@ -95,10 +96,15 @@ edit : Msg -> Item -> Item
 edit msg item = 
   case msg of 
     SetTitle title -> {item | title = title}
-    SetSize size -> item
-    SetTempo cps -> item
-    SetKey index -> item
-    SetStyle style -> item
+    SetBpm t -> 
+     let 
+        c = Maybe.withDefault item.bpm <| String.toFloat t
+        bpm = if c < bpmMin then bpmMin else if c > bpmMax then bpmMax else c
+        display = if "" == t then "0" else t
+     in 
+      { item | tempo = display, bpm = bpm }
+    SetKey index -> { item | key = index }
+    SetStyle style -> { item| style = style }
     _ -> item
 
 update : Msg -> Model -> (Model, Cmd msg) 
@@ -116,37 +122,48 @@ update msg state =
        _ -> (Viewing item, Cmd.none)
 
 
-view : Model -> msg -> msg -> msg -> (String -> msg) -> (Float -> msg) -> (Int -> msg) -> (Style -> msg) -> Html msg
-view model start finish cancel xTitle xTempo xKey xStyle =
+stub : Item -> Html msg
+stub { title, key, style, bpm } = 
+  Components.box
+   [ Components.label title
+   , text <| View.keyLabel key
+   , text <| (styleLabel style) ++ (styleInfo style)
+   , text <| View.bpmString bpm
+   ]
+
+
+view : Model -> msg -> msg -> msg -> (String -> msg) -> (String -> msg) -> (Int -> msg) -> (Style -> msg) -> Html msg
+view model start finish cancel xTitle xBpm xKey xStyle =
   let keys = [Beat, Groove, Mix, Instrumental, Abstract ] 
       select = (\i -> xStyle <| Tools.getOr i keys Mix) 
   in
   case model of 
     Viewing item -> 
       Components.box 
-        [ Components.editText "Title" (text "") item.title xTitle
-        , Components.editRange "BPM" (text "") (cpsMin, cpsMax) item.cps xTempo
-        , Components.keyPicker useSharps item.key xKey
-        , text <| Components.keyMessage useSharps item.key
+        [ stub item 
         , Components.button start [] "Edit Arc"
         ]
 
     Editing orig next ->
       Components.box 
-        [ Components.pickerSelected keys (text << styleLabel) select next.style
+        [ Components.editText "Title" (text "") next.title xTitle
+        , Components.editRangeString "BPM" (text "") (bpmMin, bpmMax) next.tempo xBpm
+        , Components.keyPickerFull useSharps next.key xKey
+        , Components.pickerSelected keys (text << styleLabel) select next.style
+        , text <|  "Applying bpm :" ++ String.fromFloat next.bpm
         , Components.button finish  [] "Save Arc"
-        , Components.button finish  [] "Cancel Changes"
+        , Components.button cancel  [] "Cancel Changes"
         ]
 
 
 init : Maybe Int -> (Model, Cmd msg)
 init flag = 
-  (Viewing new, Cmd.none)
-
+  -- (Viewing new, Cmd.none)
+  (Editing new new, Cmd.none)
 
 main = 
   Browser.element { init = init
                   , update = update
-                  , view = (\model -> view model InitEdits SaveEdits KillEdits SetTitle SetTempo SetKey SetStyle)
+                  , view = (\model -> view model InitEdits SaveEdits KillEdits SetTitle SetBpm SetKey SetStyle)
                   , subscriptions = (\_ -> Sub.none)
                   }
