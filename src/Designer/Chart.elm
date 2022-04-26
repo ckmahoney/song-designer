a7ececcae478a986ae99c720bc3afc4f13a477ed
Naltroc <cortland@invisible.college>
2022-04-26 19:40:25 -0400

    Added encoders and http methods for requesting songs

--- src/Designer/Chart.elm
+++ src/Designer/Chart.elm
@@ -5,6 +5,7 @@ import Html exposing (Html, h1, button, div, text, label, p, input,b)
 import Html.Attributes as Attr
 import Html.Events as Events exposing (onClick, onInput)
 
+import Configs
 import View 
 import Elements
 import Tools
@@ -15,7 +16,8 @@ import Card as Card
 import ScoreMeta as ScoreMeta
 import Chords
 import Json.Decode as Decode
-
+import Json.Encode as Encode
+import Http
 
 type alias CardGroup = Group.Model Card.Model
 
@@ -37,11 +39,50 @@ type Msg
   | CloseMeta
   | UpdateMeta ScoreMeta.Msg
 
+  | ReqTrack ScoreMeta.Model (List Card.Model)
+  | GotTrack (Result Http.Error ())
 
 type State 
   = Viewing ScoreMeta.Model (Group.Model Card.Model)
   | EditingCard ScoreMeta.Model (Group.Model Card.Model) Card.State
   | EditingMeta ScoreMeta.Model (Group.Model Card.Model) ScoreMeta.State 
+
+
+encodeScoreMeta : ScoreMeta.Model -> Encode.Value
+encodeScoreMeta {title, bpm, key, cpc} =
+  Encode.object
+    [ ("title", Encode.string title)
+    , ("bpm", Encode.float bpm)
+    , ("key", Encode.int key)
+    , ("cpc", Encode.int cpc)
+    ]
+
+
+encodeArc : Card.Model -> Encode.Value
+encodeArc {title, size, style}  =
+  Encode.object
+    [ ("title", Encode.string title)
+    , ("size", Encode.int size )
+    , ("style", Encode.string <| String.toLower <| Card.styleLabel style)
+    ]
+
+
+
+encodeReqTrackNext : ScoreMeta.Model -> List  Card.Model -> Encode.Value
+encodeReqTrackNext meta arcs =
+  Encode.object
+    [ ("meta", encodeScoreMeta meta)
+    , ("arcs", Encode.list encodeArc arcs)
+    ]
+
+
+reqTrack : ScoreMeta.Model -> List Card.Model -> (Result Http.Error () -> msg) -> Cmd msg
+reqTrack meta arcs complete =
+  Http.post
+    { url = Configs.apiUrl "track/next"
+    , body = Http.jsonBody <| encodeReqTrackNext meta arcs
+    , expect = Http.expectWhatever complete
+    }
 
 
 someCards : List Card.Model
@@ -63,69 +104,77 @@ init flags =
   (new, Cmd.none)
 
 
-update : Msg -> State -> (State, Cmd msg)
-update msg state = 
-  case state of 
-    Viewing meta group -> 
-      case msg of  
-        CreateCard -> 
-          (Viewing meta (Tuple.first group, List.append (Tuple.second group) [Card.create]), Cmd.none)
+update : Msg -> State -> (Result Http.Error () -> msg) -> (State, Cmd msg)
+update msg state complete = 
+  case msg of  -- "preflight" check
+    ReqTrack meta arcs ->
+      (state, reqTrack meta arcs complete)
 
-        ViewCard card -> 
-          let
-             index = Tools.findIndex card (Tuple.second group)
-             newGroup = Group.by index (Tuple.second group)
-          in 
-          (EditingCard meta newGroup <| Card.editCard card, Cmd.none)
+    GotTrack _ ->
+      (state, Cmd.none)
 
-        EditGroup gMsg -> 
-          let
-            group2 = Group.apply gMsg group
-          in
-          (Viewing meta group2, Cmd.none)
+    _ -> 
+     case state of 
+      Viewing meta group -> 
+        case msg of  
+          CreateCard -> 
+            (Viewing meta (Tuple.first group, List.append (Tuple.second group) [Card.create]), Cmd.none)
 
-        EditMeta ->
-          (EditingMeta meta group <| ScoreMeta.Editing meta meta, Cmd.none)
+          ViewCard card -> 
+            let
+               index = Tools.findIndex card (Tuple.second group)
+               newGroup = Group.by index (Tuple.second group)
+            in 
+            (EditingCard meta newGroup <| Card.editCard card, Cmd.none)
 
-        _ -> (state, Cmd.none)
+          EditGroup gMsg -> 
+            let
+              group2 = Group.apply gMsg group
+            in
+            (Viewing meta group2, Cmd.none)
 
-    EditingCard meta ((index, cards) as group) cardState -> 
-      case msg of 
-        UpdateCard cMsg ->     
-          (EditingCard meta group (Card.apply cMsg cardState), Cmd.none)
+          EditMeta ->
+            (EditingMeta meta group <| ScoreMeta.Editing meta meta, Cmd.none)
 
-        SaveCard next -> 
-          let
-            i = Maybe.withDefault -1 index
-            newGroup = (Nothing, Tools.replaceAt i next cards)
-          in
-          (Viewing meta newGroup, Cmd.none)
+          _ -> (state, Cmd.none)
 
-        CloseCard -> 
-          (Viewing meta group, Cmd.none)
+      EditingCard meta ((index, cards) as group) cardState -> 
+        case msg of 
+          UpdateCard cMsg ->     
+            (EditingCard meta group (Card.apply cMsg cardState), Cmd.none)
 
-        EditCard card -> 
-          let
-            i = Tools.findIndex card cards
-            newGroup = (Just i, cards)
-          in
-          (EditingCard meta newGroup <| Card.Editing card card, Cmd.none)
+          SaveCard next -> 
+            let
+              i = Maybe.withDefault -1 index
+              newGroup = (Nothing, Tools.replaceAt i next cards)
+            in
+            (Viewing meta newGroup, Cmd.none)
 
-        _ -> (state, Cmd.none)
+          CloseCard -> 
+            (Viewing meta group, Cmd.none)
 
-    EditingMeta orig group metaState ->
-      case msg of
-        UpdateMeta mMsg ->
-          (EditingMeta orig group (ScoreMeta.apply mMsg metaState), Cmd.none)
+          EditCard card -> 
+            let
+              i = Tools.findIndex card cards
+              newGroup = (Just i, cards)
+            in
+            (EditingCard meta newGroup <| Card.Editing card card, Cmd.none)
 
-        SaveMeta meta ->
-          (Viewing meta group, Cmd.none)
-          
-        CloseMeta ->
-          (Viewing orig group, Cmd.none)
+          _ -> (state, Cmd.none)
 
-        _ ->
-          (state, Cmd.none)
+      EditingMeta orig group metaState ->
+        case msg of
+          UpdateMeta mMsg ->
+            (EditingMeta orig group (ScoreMeta.apply mMsg metaState), Cmd.none)
+
+          SaveMeta meta ->
+            (Viewing meta group, Cmd.none)
+
+          CloseMeta ->
+            (Viewing orig group, Cmd.none)
+
+          _ ->
+            (state, Cmd.none)
 
 
 view : State -> msg -> (ScoreMeta.Msg -> msg) -> (ScoreMeta.Model -> msg) ->  msg -> (Card.Model -> msg) -> (Card.Model -> msg) -> (Card.Msg -> msg) -> (Card.Model -> msg) -> msg -> msg -> (Group.Msg Card.Model -> msg) -> Html msg
@@ -156,7 +205,7 @@ view state editMeta changeMeta saveMeta closeMeta openCard editCard change save 
 main = 
   Browser.element 
     { init = init
-    , update = update
+    , update = (\msg model -> update msg model GotTrack)
     , view = (\state -> view state EditMeta UpdateMeta SaveMeta CloseMeta ViewCard EditCard UpdateCard SaveCard CloseCard CreateCard EditGroup)
     , subscriptions = (\_ -> Sub.none)
     }
